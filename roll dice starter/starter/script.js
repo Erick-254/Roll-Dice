0eecedb2916e871405d683787c0aac0d6af25b89
Erick Rotich <erickrotich768@gmail.com>
2021-10-14 23:00:39 +0200

    Add files via upload

---
+++ roll dice starter/starter/script.js
@@ -0,0 +1,25 @@
+'use strict';
+// Selecting elements
+let score1 = document.querySelector('#score--0');  
+let score2 = document.getElementById('score--1');
+let diceE1 = document.querySelector('.dice');
+
+//additional variables
+
+
+
+// Starting conditions
+score1.textContent=0;
+score2.textContent=0;
+diceE1.classList.add('hidden');
+
+// Rolling dice functionality
+//btnRoll
+
+
+
+//btnHold
+
+
+
+//btnNew
